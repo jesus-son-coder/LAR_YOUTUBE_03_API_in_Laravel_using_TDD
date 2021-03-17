489b8f663f9b8a1a9befd58af1cda070b0f98868
rvck2007 <sekaherve@gmail.com>
2021-03-17 16:10:09 +0100

    Feature Test pour la Création d'un Produit

--- api_in_laravel_using_tdd/routes/api.php
+++ api_in_laravel_using_tdd/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::namespace('Api')->group(function() {
+    Route::post('/products', [ProductController::class, 'store']);
 });
--- api_in_laravel_using_tdd/tests/Feature/Http/Controllers/Api/ProductControllerTest.php
+++ api_in_laravel_using_tdd/tests/Feature/Http/Controllers/Api/ProductControllerTest.php
@@ -2,21 +2,51 @@
 
 namespace Tests\Feature\Http\Controllers\Api;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProductControllerTest extends TestCase
 {
+    use WithFaker;
+
     /**
-     * A basic feature test example.
-     *
      * @test
      */
     public function can_create_a_product()
     {
-        $response = $this->get('/');
+        // Given
+            // "given" : Pour créer un produit, l'utilisateur doit être authentifié.
 
-        $response->assertStatus(200);
+        // When
+            // "when" : Quand l'utilisateur réalise un "POST Request", il créer un produit.
+
+            $response = $this->json('POST', '/api/products', [
+                'name' => $name = $this->faker->company,
+                'slug' => str_slug($name),
+                'price' => $price = random_int(10,100)
+            ]);
+
+
+        // Then
+            // "then" : Il faut s'assurer que le produit créé par l'utilisateur existe.
+
+            $response->assertJsonStructure([
+                'id', 'name', 'slug', 'price', 'created_at'
+            ])
+            ->assertJson([
+                'name' => $name,
+                'slug' => str_slug($name),
+                'price' => $price
+            ])
+            ->assertStatus(201);
+
+            $this->assertDatabaseHas('products', [
+                'name' => $name,
+                'slug' => str_slug($name),
+                'price' => $price
+            ]);
+
     }
 }
+
